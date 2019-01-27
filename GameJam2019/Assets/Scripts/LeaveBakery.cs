5e1f276902eb08445d9619dc5f9abe4d8cedfbb1
Unknown <alvizoj@uci.edu>
2019-01-27 13:54:34 -0800

    Added automatic teleportation after completing puzzle
    Also added main title

---
+++ GameJam2019/Assets/Scripts/LeaveBakery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveBakery : MonoBehaviour
+{
+
+	public GameObject puzzle;
+    
+    void OnTriggerEnter(Collider other)
+    {
+    	if (puzzle.GetComponent<LightPuzzle>().isOver){
+    		SceneManager.LoadScene("City");
+    	}
+    }
+}
