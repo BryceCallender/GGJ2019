61ef452838ba1beaada7fbd79ea1f7e2d36b5405
Unknown <alvizoj@uci.edu>
2019-01-26 18:29:25 -0800

    Began city level and pause menu
    Both the city level buildings and pause menu are barebones. will add detail as I go, but they work correctly

--- GameJam2019/Assets/Scripts/UI_Manager.cs
+++ GameJam2019/Assets/Scripts/UI_Manager.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
 {
+    public GameObject[] paused_objects;
+    GameObject player;
+
+
     public void Begin_Game()
     {
-        //SceneManager.LoadScene("");
+        SceneManager.LoadScene("City");
     }
 
     public void Options()
@@ -27,5 +31,53 @@ public class UI_Manager : MonoBehaviour
     public void Quit()
     {
     	Application.Quit();
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        paused_objects = GameObject.FindGameObjectsWithTag("ShowOnPaused");
+        hidePaused();
+        player = GameObject.Find("Capsule");
+        player.GetComponent<PlayerMovement>().enabled = true;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown("escape"))
+        {
+            pause_all();
+        }
+    }
+
+    //------
+    // Helper Functions
+    //------
+    //shows objects with ShowOnPause tag
+    public void showPaused(){
+        foreach(GameObject g in paused_objects){
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused(){
+        foreach(GameObject g in paused_objects){
+            g.SetActive(false);
+        }
+    }
+
+    void pause_all()
+    {
+        if (Time.timeScale == 1){
+            Time.timeScale = 0;
+            showPaused();
+            player.GetComponent<PlayerMovement>().enabled = false;
+        }
+        else if (Time.timeScale == 0){
+            Time.timeScale = 1;
+            hidePaused();
+            player.GetComponent<PlayerMovement>().enabled = true;
+            //player.GetComponent<CharacterController>().enabled = true;
+        }
     }
 }
